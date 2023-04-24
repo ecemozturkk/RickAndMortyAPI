753f7eebcaf32a34097f63a0dac7e22cd0805332
ecemozturkk <ecemozturk704@gmail.com>
2023-04-24 22:50:55 +0300

    Initial Commit

---
+++ RickAndMortyAPI/RickAndMortyAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  RickAndMortyAPIApp.swift
+//  RickAndMortyAPI
+//
+//  Created by Ecem Öztürk on 24.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct RickAndMortyAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
